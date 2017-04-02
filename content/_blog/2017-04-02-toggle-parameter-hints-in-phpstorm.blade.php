c7ce17856ba78fede82e80e5a3184149aa03f749
Sid K <koomai@users.noreply.github.com>
2017-04-03 00:32:05 +1000

    made images responsive

--- content/_blog/2017-04-02-toggle-parameter-hints-in-phpstorm.blade.php
+++ content/_blog/2017-04-02-toggle-parameter-hints-in-phpstorm.blade.php
@@ -12,17 +12,23 @@
 
     The latest version of PhpStorm – 2017.1 includes a nifty little feature called <a href="https://blog.jetbrains.com/phpstorm/2017/03/new-in-phpstorm-2017-1-parameter-hints/" target="_blank">parameter hints</a>. I've found it very useful, especially for methods with boolean parameters (which is considered a code smell, but that is another story). 
 
-    !['Phpstorm Parameter Hints'](/assets/img/2017-04-02/parameter-hint-2.png)
+    <p>
+        <img src="/assets/img/2017-04-02/parameter-hint-1.png" alt="Phpstorm Parameter Hints" class="img-responsive">
+    </p>
 
     However, I don't want it to be enabled all the time. It's a bit of an adjustment to see the variable names inserted into your code. Fortunately, it's fairly easy to turn off the feature and use it only when needed.
 
-    Go to `PhpStorm->Preferences` and click on `Keymap`. Search for 'parameter hints' in the search field on the right hand side. You should see an option called `Toggle parameter name hints` in the results. Right-click on it, select `Add keyboard shortcut` and add your preferred shortcut. I use `Cmd + Shift + P` because it isn't assigned to anything else.
+    Go to `PhpStorm->Preferences` and click on `Keymap`. Search for *parameter hints* in the search field on the right hand side. You should see an option called `Toggle parameter name hints` in the results. Right-click on it, select `Add Keyboard Shortcut` and add your preferred shortcut. I use *Cmd + Shift + P* because it isn't assigned to anything else.
 
-    !['Phpstorm Parameter Hints'](/assets/img/2017-04-02/parameter-hint-2.png)
+    <p>
+        <img src="/assets/img/2017-04-02/parameter-hint-2.png" alt="Phpstorm Parameter Hints" class="img-responsive">
+    </p>
     
     Now you can easily toggle the feature on/off and use it only when required.
 
-     !['Phpstorm Parameter Hints'](/assets/img/2017-04-02/parameter-hint-3.gif)
+    <p>
+        <img src="/assets/img/2017-04-02/parameter-hint-3.gif" alt="Phpstorm Parameter Hints" class="img-responsive">
+    </p>
 
     @endmarkdown
 
